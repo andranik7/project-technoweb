6f30976d43517c24c3c1027fe89ee37a7ade7157
Andranik Hayrapetyan <andranikhayra@gmail.com>
2020-05-12 13:26:25 +0200

    user register route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,13 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../model/user').User
 
-/* GET users listing. */
-router.post('/', function (req, res, next) {
-  console.log(req.body.params)
-  res.json({
-    success: true,
-    message: "Bravo votre requête a marché"
+
+
+
+router.post('/register', (req, res) => {
+  let newUser = req.body
+  console.log(req.body)
+  let u = new User();
+  u.name = newUser.name;
+  u.email = newUser.email;
+  u.password = newUser.password;
+  u.save((err, user) => {
+    if (!err) {
+      res.json({
+        success: true,
+        user: user
+      })
+    } else {
+      res.json({
+        success: false,
+        message: err
+      })
+    }
   })
-});
+
+})
 
 module.exports = router;
