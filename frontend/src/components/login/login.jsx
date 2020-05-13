62b98a669c51df66e2e2eea8125323d35b0ff9c8
violetteglrd <58693006+violetteglrd@users.noreply.github.com>
2020-05-13 11:19:30 +0200

    Update login.jsx

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -9,27 +9,34 @@ export class Login extends React.Component {
     render() {
         return (
           <div className="base-container" ref={this.props.containerRef}>
-            <div className="header">Login</div>
+            <div className="header">Connexion</div>
             <div className="content">
               <div className="image">
                 <img src={loginImg} />
               </div>
               <div className="form">
                 <div className="form-group">
-                  <label htmlFor="username">Username</label>
-                  <input type="text" name="username" placeholder="username" />
+                <label htmlFor="email">Adresse e-mail</label>
+              <input type="email" name="email" placeholder="prenom.nom@isep.fr" />
                 </div>
                 <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" name="password" placeholder="password" />
+                  <label htmlFor="password">Mot de passe</label>
+                  <input type="password" name="password" placeholder="mot de passe" />
                 </div>
               </div>
             </div>
             <div className="footer">
               <button type="button" className="btn">
-                Login
+                Se connecter
               </button>
+              <div className="msg">
+                <text>Pas encore de compte ? </text>
+              </div>
+              <button type="button" className="btn">
+                S'inscrire
+              </button>  
             </div>
+         
           </div>
         );
       }
