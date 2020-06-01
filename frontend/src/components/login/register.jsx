861400ebd88ee665285f178d0384a3e5be8f386a
Andranik Hayrapetyan <andranikhayra@gmail.com>
2020-06-01 11:09:34 +0200

    radio buttons update

--- frontend/src/components/login/register.jsx
+++ frontend/src/components/login/register.jsx
@@ -5,79 +5,99 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Grid from '@material-ui/core/Grid';
 import './connexion.scss'
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 export class Register extends React.Component {
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      prenom: "",
+      nom: "",
+      email: "",
+      password: "",
+      kind: "Isepien"
     }
+  }
 
-    render() {
 
-        return (
-          //
-        <div className="container">
-          <Grid container spacing={10} direction="rows" alignItems="center" justify="center" style={{ height: '100%' }}> 
-            <Grid item md={6} >
-              <div className="image">
-                <img src={loginImg} />
-              </div>
-              </Grid>
-            <Grid item md={6} className = "scrollable">
+  handleInputChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+
+  render() {
+
+    return (
+      //
+      <div className="container">
+        <Grid container spacing={10} direction="rows" alignItems="center" justify="center" style={{ height: '100%' }}>
+          <Grid item md={6} >
+            <div className="image">
+              <img src={loginImg} />
+            </div>
+          </Grid>
+          <Grid item md={6} className="scrollable">
             <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Inscription</div>
-        <div className="content scrollable">
-          <form>
-            <div className="form-group">
-              <label htmlFor="firstname">Prénom</label>
-              <input type="text" name="firstname" placeholder="prénom" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="lastname">Nom</label>
-              <input type="text" name="lastname" placeholder="nom " />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Adresse e-mail</label>
-              <input type="email" name="email" placeholder="prenom.nom@isep.fr" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Mot de passe</label>
-              <input type="password" name="password" placeholder="mot de passe" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Confirmer mot de passe</label>
-              <input type="password" name="password" placeholder="mot de passe" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="usertype">Type d'utilisateur</label>
-            </div>
-            <div className="form-group">
-            <div className="form-group2">
-              <div><label class="radio-inline"><input type="radio" name="usertype" value="isepien"/>Isepien</label></div>
-              <div><label class="radio-inline"><input type="radio" name="usertype" value="alumni" />Alumni </label></div>
-              <div><label class="radio-inline"><input type="radio" name="usertype" value="alumni" />Autre</label></div>
-            </div>
-            </div>
-            <div className="form-group" text-align="left">
-              <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Se souvenir de moi"
-              text-align="left" />
-            </div>
-            <Button className="btn-purple" variant="primary" type="submit">S'inscrire</Button>
-            <div className="links">
-              <Link href="#">
-                {"Déjà inscrit ?"}
-              </Link>  
-          </div>
-        </form>
-        </div>
+              <div className="header">Inscription</div>
+              <div className="content scrollable">
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="firstname">Prénom</label>
+                    <input type="text" name="prenom" placeholder="prénom" onChange={this.handleInputChange} />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="lastname">Nom</label>
+                    <input type="text" name="nom" placeholder="nom " />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="email">Adresse e-mail</label>
+                    <input type="email" name="email" placeholder="prenom.nom@isep.fr" onChange={this.handleInputChange} />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="password">Mot de passe</label>
+                    <input type="password" name="password" placeholder="mot de passe" onChange={this.handleInputChange} />
+                  </div>
 
-      </div></Grid></Grid>
-          </div>
-        );
-      }
-    }
+                  <div className="form-group">
+                    <label htmlFor="usertype">Type d'utilisateur</label>
+                  </div>
+                  <div className="form-group">
+                    <div className="form-group2">
 
 
-    export default Register;
+                      <FormControl component="fieldset">
+                        <RadioGroup aria-label="kind" name="kind" value={this.state.kind} onChange={this.handleInputChange}>
+                          <FormControlLabel value="Isepien" control={<Radio />} label="Isepien" />
+                          <FormControlLabel value="Alumni" control={<Radio />} label="Alumni" />
+                          <FormControlLabel value="Professeur" control={<Radio />} label="Professeur" />
+                        </RadioGroup>
+                      </FormControl>
+                    </div>
+                  </div>
+                  <div className="form-group" text-align="left">
+                    <FormControlLabel
+                      control={<Checkbox value="remember" color="primary" />}
+                      label="Se souvenir de moi"
+                      text-align="left" />
+                  </div>
+                  <Button className="btn-purple" variant="primary" type="submit">S'inscrire</Button>
+                  <div className="links">
+                    <Link href="#">
+                      {"Déjà inscrit ?"}
+                    </Link>
+                  </div>
+                </form>
+              </div>
+
+            </div></Grid></Grid>
+      </div>
+    );
+  }
+}
+
+
+export default Register;
